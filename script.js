09f863a7928daf1d5c62d2964ef9855365393dc4
FedericoCarrasco <federicogcarrasco@gmail.com>
2021-11-02 00:46:54 -0300

    Add some books to the array for testing

--- script.js
+++ script.js
@@ -1,16 +1,65 @@
-const library = [];
+const library = [
+    {
+        title: `Harry Potter and the Sorcerer's Stone`,
+        author: `J.K. Rowling`,
+        pages: 345,
+        read: 'Not yet',
+    },
+    {
+        title: `Harry Potter and the Chamber of Secrets`,
+        author:`J.K. Rowling`,
+        pages: 141,
+        read: `Not yet`,
+    },
+    {
+        title: `Harry Potter and the Prisoner of Azkaban`,
+        author:`J.K. Rowling`,
+        pages: 180,
+        read: `Not yet`,
+    },
+    {
+        title: `Harry Potter and the Goblet of Fire`,
+        author:`J.K. Rowling`,
+        pages: 301,
+        read: `Not yet`,
+    },
+    {
+        title: `Harry Potter and the Order of the Phoenix`,
+        author:`J.K. Rowling`,
+        pages: 412,
+        read: `Not yet`,
+    },
+    {
+        title: `Harry Potter and the Half-Blood Prince`,
+        author:`J.K. Rowling`,
+        pages: 652,
+        read: `Not yet`,
+    },
+    {
+        title: `Harry Potter and the Deathly Hallows`,
+        author:`J.K. Rowling`,
+        pages: 316,
+        read: `Not yet`,
+    },
+    {
+        title: `Harry Potter and the Cursed Child`,
+        author:`J.K. Rowling`,
+        pages: 336,
+        read: `Not yet`,
+    }
+];
 
-function Book (title, author, pages, read) {
+function Book (title, author, pages, readed) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = read
+    this.readed = readed
     this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`
+        return `${title} by ${author}, ${pages} pages, ${readed}`
     }
 }
 
-function addBookToLibrary (title, author, pages, read) {
-    let newBook = new Book(title, author, pages, read)
+function addBookToLibrary (title, author, pages, readed) {
+    let newBook = new Book(title, author, pages, readed)
     library.push(newBook)
 }
